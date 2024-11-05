5ea6444b4f3f0b8a39208d2c297ec077d823f063
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-11-05 15:40:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Support and contact information - Provide your own support channels
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/world_friend_chatting_zone")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sunlight_world")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/music_support4")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
