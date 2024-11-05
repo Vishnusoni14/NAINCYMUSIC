cd6930fbd7741f8eed37f8d902c80a5cd594a58c
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-11-05 19:12:31 +0530

    Update botschk.py

--- ANNIEMUSIC/plugins/sudo/botschk.py
+++ ANNIEMUSIC/plugins/sudo/botschk.py
@@ -8,11 +8,7 @@ from config import OWNER_ID
 userbot = Userbot()
 
 BOT_LIST = [
-    "Ava_The_RoBot",
-    "Strings_Gen_Bot",
-    "TheFlashRobot",
-    "AnnieMusicRobot",
-    "String_Phish_Robot"
+    "moonlight_musicRobot",
 ]
 
 @app.on_message(filters.command("botschk") & filters.group)
